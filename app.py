71dda95934876211f86380be0efe8e9a09a16eb0
NandoAACF <nando.aacf88@gmail.com>
2023-07-20 17:34:06 +0700

    feat: add page information and page insight

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 from page_cluster import show_cluster
 from page_information import show_information
+from page_visualisasi import show_visualisasi
+from page_insight_for_perusahaan import show_insight_perusahaan
 
-page = st.sidebar.selectbox("Choose Option", ("Cluster", "Information"))
+page = st.sidebar.selectbox("Choose Option", ("Cluster", "Information", "Visualisasi", "Insight untuk Perusahaan"))
 
 if page == 'Cluster':
     show_cluster()
 elif page == 'Information':
     show_information()
+elif page == 'Visualisasi':
+    show_visualisasi()
+elif page == 'Insight untuk Perusahaan':
+    show_insight_perusahaan()
